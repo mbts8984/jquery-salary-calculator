1c4d32226ada836a23912cbdc476d8ffb17efa5a
Madeline Morton <madelinemorton614@gmail.com>
2019-04-14 11:04:08 -0500

    added gitignore file, updated HTML and added function so that input fields clear

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ console.log('in JS');
 
 $(document).ready( handleReady );
 
-employeeData = [];
+let employeeData = []; //array that stores newEmployeeObject inside it for global reference
+let totalAnnualSalaries = 0; // setting global numeric variable to calculate across multiple functions
 
+//making sure jQuery is on and running the needed funtions for when the page is loaded
 function handleReady () {
     console.log('jQuery is on');   
     buttonClick();
@@ -17,10 +19,40 @@ function handleReady () {
      //end buttonClick
  }
 
+ // function grabbing input information from the fields and pushing them into the newEmployeeObject array. 
 function addNewEmployee(){
     console.log('in addNewEmployee');
+    const newEmployeeObject = {
+        firstName: $('#firstNameInput').val(),
+        lastName: $('#lastNameInput').val(),
+        employeeID: $('#employeeIdInput').val(),
+        title: $('#titleInput').val(),
+        annualSalary: $('#annualSalaryInput').val()
+    }
+    employeeData.push(newEmployeeObject);
+    $('#firstNameInput').val(''),
+    $('#lastNameInput').val(''),
+    $('#employeeIdInput').val(''),
+    $('#titleInput').val(''),
+    $('#annualSalaryInput').val('')
+    displayObject();
+}
+// clear input values
+
+
+function displayObject(){
+    //$('#tBody').append(`
+    let newObject = employeeData[employeeData.length-1];
+    //append new employee data into employeeData array and display in the table
+    $('#tBody').append(`    
+        <tr>
+            <td>${newObject.firstName}</td>
+            <td>${newObject.lastName}</td>
+            <td>${newObject.employeeID}</td>
+            <td>${newObject.title}</td>
+            <td>${newObject.annualSalary}</td>
+        </tr>    
+        `)
 }
 
-const newEmployeeObject = {
-    
-}
+
